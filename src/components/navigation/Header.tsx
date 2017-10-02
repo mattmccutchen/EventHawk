5cf648449dc0cf72653e007f5ae8037482518df1
Isaac Pittman <Isaac_Pittman@student.uml.edu>
2017-10-01 20:11:22 -0400

    Cleanup comments

--- src/components/navigation/Header.tsx
+++ src/components/navigation/Header.tsx
@@ -11,27 +11,15 @@ export class Header extends React.Component {
                 </Navbar.Brand>
 
             </Navbar.Header>
-            
+
             <Nav pullRight>
-                    <NavItem className="options-container">
-                        <Link to="login" className="option">Sign In</Link>
-                    </NavItem>
-                    <NavItem className="options-container">
-                        <Link to="register" className="option">Sign Up</Link>
-                    </NavItem>
-                </Nav>
+                <NavItem className="options-container">
+                    <Link to="login" className="option">Sign In</Link>
+                </NavItem>
+                <NavItem className="options-container">
+                    <Link to="register" className="option">Sign Up</Link>
+                </NavItem>
+            </Nav>
         </Navbar>
     }
-    /* ReactDOM.render(navbarInstance, mountNode);*/
-    /*
-        render() {
-            return <nav className="header">
-                <Link to="/" className="navbar-brand header-logo">EventHawk</Link>
-                <div className="options-container">
-                    <Link to="login" className="option">Sign In</Link>
-                    <Link to="register" className="option">Sign Up</Link>
-                </div>
-            </nav>
-            }
-        */
 }
