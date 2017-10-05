d4d80505d3d53d702fc4d1c11530ab87ab7d4e74
Saurabh Verma <17792518+sv-uml@users.noreply.github.com>
2017-10-05 16:27:11 -0400

    Update package.json to add support for React 16

--- src/components/navigation/Sidebar.tsx
+++ src/components/navigation/Sidebar.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import { NavLink } from "react-router-dom"
 export class Sidebar extends React.Component {
     render() {
-        return <div className={`sidebar left`}>
+        return <div className="sidebar left">
             <ul>
                 <li><NavLink to={`/`} exact activeClassName="active"><i className="fa fa-rss" aria-hidden="true"></i> Stream</NavLink></li>
                 <li><NavLink to={`/myevent`}><i className="fa fa-calendar-o" aria-hidden="true"></i> My Events</NavLink></li>
