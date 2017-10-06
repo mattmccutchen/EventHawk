8e3e5f51e242ba0973f2dea68d0f17ed082d8df5
Isaac Pittman <Isaac_Pittman@student.uml.edu>
2017-10-05 21:20:35 -0400

    Dynamically generate Event List items

--- src/components/events/EventList.tsx
+++ src/components/events/EventList.tsx
@@ -5,55 +5,68 @@ import { Link } from "react-router-dom";
 
 
 interface State {
-    isEventExpanded: boolean;
+    expandedEventId: string;
 }
 
 export class EventList extends React.Component<any, State> {
 
     constructor(props: any) {
         super(props);
-        this.state = { isEventExpanded: true };
+
+        this.state = { expandedEventId: null }
+    }
+
+    handleListGroupItemClick(key: string) {
+        if (this.state.expandedEventId === key) {
+            // If this item is already expanded, collapse it.
+            this.setState({ expandedEventId: null });
+        } else {
+            this.setState({ expandedEventId: key })
+        }
+    }
+
+    createListGroupItem(key: string) {
+        return (
+            <div>
+                <ListGroupItem key={key} onClick={() => this.handleListGroupItemClick(key)}>
+                    <Grid>
+                        <Row>
+                            <Col xs={1}>
+                                <Badge>13</Badge>
+                            </Col>
+                            <Col xs={2}>
+                                My Baseball Event
+                    </Col>
+                            <Col xs={1}>
+                                <Glyphicon glyph="arrow-up" />
+                                <Glyphicon glyph="arrow-down" />
+                            </Col>
+                            <Col xs={1}>
+                                <Glyphicon glyph="ok-circle" />
+                            </Col>
+                        </Row>
+                    </Grid>
+                </ListGroupItem>
+
+                <Panel collapsible expanded={this.state.expandedEventId === key}>
+                    <Well>
+                        <div>Host: <Link to="/users/profile">John A. Student</Link></div>
+                        <div>Description: Leeeetttttssss play ball!</div>
+                    </Well>
+                </Panel>
+            </div>
+        );
     }
 
     render() {
         return (
             <div>
                 <ListGroup>
-                    <ListGroupItem onClick={() => this.setState({ isEventExpanded: !this.state.isEventExpanded })}>
-                        <Grid>
-                            <Row>
-                                <Col xs={1}>
-                                    <Badge>13</Badge>
-                                </Col>
-                                <Col xs={2}>
-                                    My Baseball Event
-                                </Col>
-                                <Col xs={1}>
-                                    <Glyphicon glyph="arrow-up" />
-                                    <Glyphicon glyph="arrow-down" />
-                                </Col>
-                                <Col xs={1}>
-                                    <Glyphicon glyph="ok-circle" />
-                                </Col>
-                            </Row>
-                        </Grid>
-                    </ListGroupItem>
-                    <Panel collapsible expanded={this.state.isEventExpanded}>
-                        <Well>
-                            <div>Host: <Link to="/users/profile">John A. Student</Link></div>
-                            <div>Description: Leeeetttttssss play ball!</div>
-                        </Well>
-                    </Panel>
-                    <ListGroupItem>
-                        Event 2
-                    </ListGroupItem>
-                    <ListGroupItem>
-                        Event 3
-                    </ListGroupItem>
-
-                    <LinkContainer to="/events/create">
-                        <ListGroupItem>Create Event</ListGroupItem>
-                    </LinkContainer>
+                    {this.createListGroupItem("a")}
+                    {this.createListGroupItem("b")}
+                    {this.createListGroupItem("c")}
+                    {this.createListGroupItem("d")}
+                    {this.createListGroupItem("e")}
                 </ListGroup>
             </div>
         );
