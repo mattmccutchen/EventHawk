6842a424606c6334e2cab9aece71e0f042e1e330
Saurabh Verma <17792518+sv-uml@users.noreply.github.com>
2017-10-09 19:25:30 -0400

    Responsive design enhancements

--- src/components/navigation/Header.tsx
+++ src/components/navigation/Header.tsx
@@ -2,10 +2,19 @@ import * as React from "react";
 import { Link } from "react-router-dom";
 import { Navbar, Nav, NavItem, MenuItem, NavDropdown } from "react-bootstrap";
 import { Login } from "../authentication/Login";
+import { Sidebar } from "./Sidebar"
 
 export class Header extends React.Component {
     render() {
         return <nav className="header">
+            <div id="side_navbar">
+                <input type="checkbox" />
+                <i className="fa fa-bars"></i>
+                <i className="fa fa-times"></i>
+                <ul id="side_nav_dropin">
+                    <Sidebar />
+                </ul>
+            </div>
             <Link to="/" className="navbar-brand header-logo">EventHawk</Link>
             <div className="options-container">
                 <div className="links">
