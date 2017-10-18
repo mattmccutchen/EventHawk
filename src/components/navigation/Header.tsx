11d28440aa7f9111281c276e2a94bcb7b97fc440
Saurabh Verma <17792518+sv-uml@users.noreply.github.com>
2017-10-17 23:51:58 -0400

    Fix page scroll and sidebar behavior

--- src/components/navigation/Header.tsx
+++ src/components/navigation/Header.tsx
@@ -2,19 +2,17 @@ import * as React from "react";
 import { Link } from "react-router-dom";
 import { Navbar, Nav, NavItem, MenuItem, NavDropdown } from "react-bootstrap";
 import { Login } from "../authentication/Login";
-import { Sidebar } from "./Sidebar"
+import { DropSidebar } from "./DropSidebar"
 
 export class Header extends React.Component {
+
+    constructor(props: any) {
+        super(props);
+    }
+
     render() {
         return <nav className="header">
-            <div id="side_navbar">
-                <input type="checkbox" />
-                <i className="fa fa-bars"></i>
-                <i className="fa fa-times"></i>
-                <ul id="side_nav_dropin">
-                    <Sidebar />
-                </ul>
-            </div>
+            <DropSidebar />
             <Link to="/" className="navbar-brand header-logo">EventHawk</Link>
             <div className="options-container">
                 <div className="links">
