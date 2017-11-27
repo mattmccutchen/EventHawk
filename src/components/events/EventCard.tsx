1c694655ba5b7dc0cadc134b61c1e965b7508101
Saurabh Verma <17792518+sv-uml@users.noreply.github.com>
2017-11-26 22:15:07 -0500

    Event Card ratings

--- src/components/events/EventCard.tsx
+++ src/components/events/EventCard.tsx
@@ -28,19 +28,43 @@ export interface EventCardProps {
     handleRateClick?: () => void,
 }
 
-export class EventCard extends React.Component<EventCardProps, {}> {
+interface IEventCardState {
+    reviewOpen: boolean
+}
+
+export class EventCard extends React.Component<EventCardProps, IEventCardState> {
 
     constructor(props: EventCardProps) {
         super(props);
+        this.state = {
+            reviewOpen: false
+        }
+        this.renderReview = this.renderReview.bind(this);
+        this.renderReviewCard
+    }
+
+    renderReview() {
+        if (!this.state.reviewOpen) {
+            this.setState({ reviewOpen: !this.state.reviewOpen });
+            document.addEventListener("click", this.renderReview);
+        } else {
+            this.setState({ reviewOpen: !this.state.reviewOpen });
+            document.removeEventListener("click", this.renderReview);
+        }
     }
 
     renderVoteButtons() {
+        let spotsLeft: number = (this.props.capacity - this.props.currentCapacity);
         // Event hosts cannot vote on their own events, so hide the voting buttons for events hosted
         // by the logged in user
         if (this.props.isHostedByCurrentUser) {
             return (
                 <div className="event-options-panel">
                     <span className="event-votes">{this.props.interest}</span>
+                    <div className="spots">
+                        <span className="spots-left">{spotsLeft}</span>
+                        <span className="num-spots-label">Spots left</span>
+                    </div>
                 </div>
             )
         }
@@ -50,6 +74,10 @@ export class EventCard extends React.Component<EventCardProps, {}> {
                 <span className="event-upvote" style={this.props.vote == 1 ? { color: "green" } : null} onClick={this.props.handleUpvote}><i className="fa fa-arrow-up"></i></span>
                 <span className="event-votes">{this.props.interest}</span>
                 <span className="event-downvote" style={this.props.vote == -1 ? { color: "green" } : null} onClick={this.props.handleDownvote}><i className="fa fa-arrow-down"></i></span>
+                <div className="spots">
+                    <span className="num-spots-left">{spotsLeft}</span>
+                    <span className="num-spots-label">Spots left</span>
+                </div>
             </div>
         )
     }
@@ -88,7 +116,7 @@ export class EventCard extends React.Component<EventCardProps, {}> {
         }
     }
 
-    renderReviews() {
+    renderReviewCard() {
         const hostPrepTooltip = (
             <Tooltip id="hostPrepTooltip"><div><strong>Host preparedness</strong></div> Was the host prepared? From 1 (worst) to 5 (best)</Tooltip>
         );
@@ -100,28 +128,27 @@ export class EventCard extends React.Component<EventCardProps, {}> {
         );
 
         return (
-            <div>
-                Reviews:
-                <OverlayTrigger placement="top" overlay={hostPrepTooltip}>
-                    <Label>{this.props.reviewHostPrep}</Label>
-                </OverlayTrigger>
-                <OverlayTrigger placement="top" overlay={matchedDescTooltip}>
-                    <Label>{this.props.reviewMatchedDesc}</Label>
-                </OverlayTrigger>
-                <OverlayTrigger placement="top" overlay={wouldReturnTooltip}>
-                    <Label>{this.props.reviewWouldReturn}</Label>
-                </OverlayTrigger>
+            <div className="event-review-container">
+                <div className="event-review-stat">
+                    <span className="event-review-num">{this.props.reviewHostPrep}</span>
+                    <span className="event-review-label">Host Preparedness</span>
+                </div>
+                <div className="event-review-stat">
+                    <span className="event-review-num">{this.props.reviewMatchedDesc}</span>
+                    <span className="event-review-label">Matched Description</span>
+                </div>
+                <div className="event-review-stat">
+                    <span className="event-review-num">{this.props.reviewWouldReturn}</span>
+                    <span className="event-review-label">Would Return</span>
+                </div>
             </div>
         )
     }
 
     render() {
-        let items: dropdownItem[] =
-            [{
-                label: "Statistics", link: "/event/" + this.props.eventid + "/statistics"
-            }];
-
-        let spotsLeft: number = (this.props.capacity - this.props.currentCapacity);
+        let items: dropdownItem[] = [{
+            label: "Statistics", link: "/event/" + this.props.eventid + "/statistics"
+        }];
         return <div className="event-item-container">
             {this.renderVoteButtons()}
             <div className="event-content-panel">
@@ -132,15 +159,23 @@ export class EventCard extends React.Component<EventCardProps, {}> {
                         <Dropdown className="" items={items}><i className="fa fa-angle-down"></i></Dropdown>
                     </div>
                 </div>
-                <div className="event-host">{this.props.host}{this.renderReviews()}</div>
+                <div className="event-host">{this.props.host}</div>
                 <div className="event-description">{this.props.description}</div>
                 <div className="event-location-attend">
                     <div className="event-location">Location: {this.props.location}</div>
                     {this.renderAttending()}
                 </div>
+                <span className="separator" />
                 <div className="event-stats">
                     <span className="event-time">{this.props.time.format("dddd, MMM Do YYYY, h:mm A")}</span>
-                    <span className="event-going"><strong>{spotsLeft}</strong> spots left</span>
+                    <span className="event-reviews">Reviews 
+                        {this.state.reviewOpen ? <i className="fa fa-angle-right" onClick={this.renderReview}></i> : 
+                        <i className="fa fa-angle-down" onClick={this.renderReview}></i>}
+                    </span>
+                </div>
+                <div className={ "event-review-content " + (this.state.reviewOpen ? "open" : "")}>
+                    <span className="separator" />
+                    {this.renderReviewCard()}
                 </div>
             </div>
         </div>;
